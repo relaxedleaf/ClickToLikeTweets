491236d003a547a190634ac89d4ed0b59662dd7e
Guanghui Li <liguanghui.cs@gmail.com>
2023-02-01 19:19:13 -0600

    navbar stash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import store from '../store';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
 			<ChakraProvider>
+				<Navbar />
 				<main>
 					<Component {...pageProps} />
 				</main>
